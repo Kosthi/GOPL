5a4efdad96006d0089b37e4eb384caaa431e7021
Koschei <nitianzero@gmail.com>
2023-07-28 21:22:32 +0800

    feat: enable to write and read file

--- ch1/dup/main.go
+++ ch1/dup/main.go
@@ -22,21 +22,23 @@ func main() {
 	counts := make(map[string]int)
 	line2file := make(map[string]string)
 
-	//if len(os.Args) == 1 {
-	//	var fileName = "output.txt"
-	//	f, err := os.Open(fileName)
-	//	if err != nil {
-	//		log.Fatal("Wrong in open file.")
-	//	}
-	//	fmt.Println("Input file content.")
-	//	reader := bufio.NewScanner(os.Stdin)
-	//	for reader.Scan() {
-	//		f.Write(reader.Bytes())
-	//	}
-	//}
-
 	if len(os.Args) == 1 {
-		countLines(os.Stdin, counts, line2file)
+		var fileName = "input.txt"
+		f, err := os.Create(fileName)
+		if err != nil {
+			log.Fatal("Wrong in open file.")
+		}
+		fmt.Println("Input file content.\nEnter \"#\" to exit")
+		reader := bufio.NewScanner(os.Stdin)
+		for reader.Scan() {
+			if reader.Text() == "#" {
+				break
+			}
+			f.WriteString(reader.Text() + "\n")
+		}
+		f.Seek(0, 0)
+		countLines(f, counts, line2file)
+		f.Close()
 	} else {
 		for _, arg := range os.Args[1:] {
 			f, err := os.Open(arg)
